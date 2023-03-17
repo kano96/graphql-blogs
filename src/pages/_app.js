74868fa40ac3862018a819e9dc7fe8233c080676
Kevin Torres <kevin.torres@devsu.com>
2023-03-17 12:13:07 -0500

    add categories and detail post scenes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.scss";
 import { useEffect, useState } from "react";
-import { Layout } from "./components";
+import { Layout } from "../components";
 
 export default function App({ Component, pageProps }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { PostCard, PostWidget, Categories } from "./components";
+import { PostCard, PostWidget, Categories } from "../components";
 import { getPosts } from "../../services";
 
 export default function Home({ posts }) {
